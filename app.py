14d03c571c5dd6f3c2528ec02b1b626885a31570
fathiardiann <fathiardiann@gmail.com>
2025-11-10 16:21:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ model, df = load_model()
 st.markdown("""
 <h1 style='text-align:center; line-height:1.2;'>
 🏠 Prediksi Harga Rumah California<br>
-<span>Menggunakan Linear Regression</span>
+<span>   Menggunakan Linear Regression</span>
 </h1>
 """, unsafe_allow_html=True)
 
@@ -243,6 +243,7 @@ with col_right:
 # ---------------------------
 st.markdown("---")
 st.markdown("<div style='text-align:center; color:#9aa3b2; font-size:12px'>© 2025 California Housing Predictor</div>", unsafe_allow_html=True)
+
 
 
 
